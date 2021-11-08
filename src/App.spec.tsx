f3df022ddddd2177bd9a5e439e548aa5760d9f97
kkosmowski <krzysztof.kosmowski@netguru.com>
2021-11-08 15:48:05 +0100

    feat(routing): add a test for category routing

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -4,9 +4,18 @@ import store from './store/store';
 import { AnyAction } from 'redux';
 import { getMockedNote } from './utils/get-mocked-note.util';
 import NoteActions from './store/actionCreators/note.action-creators';
-import { ProvidedApp } from './App';
+import { App, ProvidedApp } from './App';
 import { render, screen, waitFor } from '@testing-library/react';
-import { noteTestId } from './domain/consts/test-ids.consts';
+import { categoryTitleTestId, noteTestId } from './domain/consts/test-ids.consts';
+import { MemoryRouter, Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { Main } from './Main/Main';
+import { mockStore } from './utils/mock.store';
+import { initialRootState } from './utils/initial-root-state';
+import { initialCategoryState } from './store/reducers/category.reducer';
+import { createMemoryHistory } from 'history';
+import { MockStoreEnhanced } from 'redux-mock-store';
+import { RootState } from './store/interfaces/root-state.interface';
 
 describe('Category', () => {
   it('selecting a category filters notes assigned to this category', () => {
@@ -35,6 +44,28 @@ describe('Category', () => {
               .catch();
           })
           .catch();
+      })
+      .catch();
+  });
+
+  it('is correctly selected when entering a specific URL', async () => {
+    const mockedCategory = getMockedCategory({ name: 'Different name' });
+    const categoryRoute = `/category/${ mockedCategory.id }`;
+
+    render(
+      <MemoryRouter initialEntries={ [categoryRoute] }>
+        <Provider store={ store }>
+          <Main />
+        </Provider>
+      </MemoryRouter>
+    );
+
+    return store
+      .dispatch(CategoryActions._getSuccess([mockedCategory]) as unknown as AnyAction)
+      .then(async () => {
+        await waitFor(() => {
+          expect(screen.queryByTestId(categoryTitleTestId)).toHaveTextContent(mockedCategory.name);
+        });
       })
       .catch();
   });
--- src/utils/get-mocked-category.util.ts
+++ src/utils/get-mocked-category.util.ts
@@ -1,8 +1,9 @@
 import { v4 } from 'uuid';
 import { Category } from '../domain/interfaces/category.interface';
 
-export const getMockedCategory = (): Category => ({
+export const getMockedCategory = (partial: Partial<Category> = {}): Category => ({
   id: v4(),
   name: 'Mocked category',
   deleted: false,
+  ...partial,
 });
