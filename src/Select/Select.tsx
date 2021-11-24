121ba46c3701c13d4f421617d0972db95a398e7f
kkosmowski <krzysztof.kosmowski@netguru.com>
2021-11-24 18:41:07 +0100

    feat(select): customize select

--- src/Select/Select.tsx
+++ src/Select/Select.tsx
@@ -1,69 +1,66 @@
-import { ChangeEvent, ReactElement, useEffect, useState } from 'react';
+import { MouseEvent, ReactElement, useEffect, useState } from 'react';
 import { SelectOption } from './select-option.interface';
-import styled from 'styled-components/macro';
 import { Label } from '../Label/Label';
-import { InputStyles } from '../Input/Input.styled';
 import { useTranslation } from 'react-i18next';
+import { v4 as uuidv4 } from 'uuid';
+import { Backdrop } from '../Backdrop/Backdrop';
+import {
+  CurrentOption,
+  CustomSelect,
+  Option,
+  OptionsContainer,
+  SelectWrapper
+} from './Select.styled';
 
 interface Props {
   label?: string;
   options: SelectOption[];
-  initialValue?: string | number;
-  onChange: (e: ChangeEvent<HTMLSelectElement>) => void;
+  initialValue?: string;
+  multi?: boolean;
+  onChange: (value: string) => void;
 }
 
-export const Select = ({ options, label, initialValue, onChange }: Props): ReactElement => {
-  const [value, setValue] = useState(initialValue);
+export const Select = ({ options, label, initialValue, multi, onChange }: Props): ReactElement => {
+  const id = uuidv4();
+  const [value, setValue] = useState<string | undefined>(initialValue);
+  const [optionsVisible, setOptionsVisible] = useState<boolean>(false);
   const { t } = useTranslation();
 
   useEffect(() => { setValue(initialValue); }, [initialValue]);
 
-  const handleChange = (e: ChangeEvent<HTMLSelectElement>): void => {
-    setValue(e.target.value);
-    onChange(e);
+  const handleOptionClick = (e: MouseEvent<HTMLElement>): void => {
+    const selectedValue = (e.target as HTMLElement).dataset?.value;
+    if (selectedValue) {
+      setValue(selectedValue);
+      onChange(selectedValue);
+    }
+  };
+
+  const toggleOptionsVisibility = (): void => {
+    setOptionsVisible(!optionsVisible);
   };
 
   return (
-    <SelectWrapper>
-      { label && <Label>{ label }</Label> }
-      <StyledSelect onChange={ handleChange } value={ value }>
-        { options.map(({ label, value, translate }) => (
-          <option value={ value } key={ value }>
-            { translate ? t(label) : label }
-          </option>
-        ))}
-      </StyledSelect>
-    </SelectWrapper>
+    <>
+      <SelectWrapper opened={ optionsVisible }>
+        { label && <Label>{ label }</Label> }
+        <CustomSelect onClick={ toggleOptionsVisibility }>
+          <CurrentOption>{ value && t(options.find(o => o.value === value)!.label) }</CurrentOption>
+          <OptionsContainer visible={ optionsVisible }>
+            { options.map(({ label, value, translate }) => (
+              <Option
+                onClick={ handleOptionClick }
+                data-parent={ id }
+                data-value={ value }
+                key={ value }
+              >
+                { translate ? t(label) : label }
+              </Option>
+            ))}
+          </OptionsContainer>
+        </CustomSelect>
+      </SelectWrapper>
+      { optionsVisible && <Backdrop onClick={ toggleOptionsVisibility } /> }
+    </>
   );
 };
-
-const SelectWrapper = styled.div`
-  position: relative;
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-
-  &::after {
-    position: absolute;
-    content: '';
-    width: 8px;
-    height: 8px;
-    color: var(--foreground-61);
-    border-right: 1px solid;
-    border-bottom: 1px solid;
-    right: 10px;
-    bottom: 15px;
-    transform: rotate(45deg);
-  }
-`;
-
-const StyledSelect = styled.select`
-  appearance: none;
-  ${ InputStyles };
-  padding: 4px 32px 4px 20px;
-  min-width: 80px;
-  
-  @media (max-width: 599px) {
-    width: 100%;
-  }
-`;
