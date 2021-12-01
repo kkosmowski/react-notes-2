13b6f9ecb2db03ff7beddf40b23351278c5db235
kkosmowski <krzysztof.kosmowski@netguru.com>
2021-12-01 19:27:26 +0100

    fix(masonry): fix masonry when there is only a handful of notes feat(note): improve notes styles

--- src/NotesContainer/NotesContainer.styled.ts
+++ src/NotesContainer/NotesContainer.styled.ts
@@ -1,4 +1,7 @@
 import styled from 'styled-components/macro';
+import { Masonry } from '@mui/lab';
+import { ResponsiveStyleValue } from '@mui/system';
+import { COLUMN_MAX_WIDTH_PX } from '../domain/consts/note-container.consts';
 
 export const NotesWrapper = styled.div`
   width: 100%;
@@ -12,4 +15,15 @@ export const NotesWrapperContainer = styled.section`
 
 export const NoNotesText = styled.p`
   color: var(--foreground-61);
+`;
+
+export const StyledMasonry = styled(Masonry)<{ columns?: ResponsiveStyleValue<number | string> }>`
+  ${ ({ columns }) => {
+    if (typeof columns === 'number') {
+      if (columns * COLUMN_MAX_WIDTH_PX < window.innerWidth) {
+        return `width: ${ columns * COLUMN_MAX_WIDTH_PX }px;`;
+      }
+    }
+    return '';
+  } }
 `;
--- src/domain/consts/note.consts.ts
+++ src/domain/consts/note.consts.ts
@@ -1,4 +1,4 @@
-export const MAX_VISIBLE_TITLE_LENGTH = 32;
-export const MAX_VISIBLE_CONTENT_LENGTH = 64;
+export const MAX_VISIBLE_TITLE_LENGTH = 42;
+export const MAX_VISIBLE_CONTENT_LENGTH = 128;
 
 export const MAX_TITLE_LENGTH = 120;
